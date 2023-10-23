a2fb0b39da6cb3f1a378ef3a907a4890c5bc6d2c
Alishba Laeeq <111296500+alishbalaeeq@users.noreply.github.com>
2023-10-23 17:53:46 +0500

    Update functions.groovy

--- functions.groovy
+++ functions.groovy
@@ -1,11 +1,20 @@
 def deploy(String branch_name) {
-  if (branch_name == "PROD") {
+  if (branch_name == "production") {
     println "Deploying to Production."
-  } else if (branch_name == "TEST") {
+  } else if (branch_name == "test") {
     println "Deploying to Test."
   } else {
     println "Deploying to Dev."
   }
+}
+
+def build() {
+  echo 'Building'
+  bat 'pip3 install -r requirements.txt'
+}
+
+def checkout() {
+  git branch: 'main', url: 'https://github.com/alishbalaeeq/FlaskApp.git'
 }
 
   return this
